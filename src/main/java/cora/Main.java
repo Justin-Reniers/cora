d22770c04edb55268dcd27fd4b46ca71e9b423d1
Justin Reniers <justin-reniers@hotmail.com>
2019-11-07 17:23:29 +0100

    Setting up project
    Fixing project structure to get it up and running.

--- src/main/java/cora/Main.java
+++ src/main/java/cora/Main.java
@@ -43,7 +43,7 @@ public class Main {
 
   public static void main(String[] args) {
     try {
-      TRS trs = args.length > 0 ? readInput(args[0]) : readInput("test.cora");
+      TRS trs = args.length > 0 ? readInput(args[0]) : readInput("test.trs");
       if (trs == null) return;
 
       System.out.print(trs.toString());
