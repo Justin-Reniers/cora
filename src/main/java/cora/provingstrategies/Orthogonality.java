24b1e9ec74c6d850ba2d81a4fda45085bc1b69b6
Justin Reniers <justin-reniers@hotmail.com>
2019-11-28 19:15:01 +0100

    Checking all subterms and using fresh variables
    Orthogonality now checks all subterms instead of just "first level" subterms and uses fresh variables for first rule to catch self-overlap edge cases.

--- src/main/java/cora/provingstrategies/Orthogonality.java
+++ src/main/java/cora/provingstrategies/Orthogonality.java
@@ -4,6 +4,7 @@ import cora.exceptions.NullCallError;
 import cora.interfaces.provingstrategies.Strategy;
 import cora.interfaces.rewriting.TRS;
 import cora.interfaces.terms.*;
+import cora.rewriting.FirstOrderRule;
 import cora.terms.Subst;
 import cora.terms.Var;
 import java.util.ArrayList;
@@ -29,68 +30,6 @@ public class Orthogonality extends StrategyInherit implements Strategy {
         return true;
     }
 
-    /**
-     * Tries to find overlap between terms by trying to match them. If the terms share a root symbol we attempt to
-     * match the complete terms. Else we try to match t2 with a non-variable subterm of t1. No matching results in
-     * it returning null, as there is no overlap.
-     */
-    private Substitution overlap(Term t1, Term t2) {
-        if (t1.isFunctionalTerm() && t2.isFunctionalTerm() && t1.queryRoot().equals(t2.queryRoot()))
-            try {
-                return t1.match(t2);
-            } catch (NullCallError e) {
-                e.getMessage();
-            }
-        for (int i = 1; i <= t1.numberImmediateSubterms(); i++) {
-            if (!t1.queryImmediateSubterm(i).isVariable())
-                try {
-                    return (Subst) t1.queryImmediateSubterm(i).match(t2);
-                } catch (NullCallError e) {
-                    e.getMessage();
-                }
-        }
-        return null;
-    }
-
-    /**
-     * Attempts to find a critical pair between the two rules of trs at rule index 1 and rule index 2. Pairs are
-     * found by matching the left term of rule 2 with a non-variable subterm of t1 or t1 in whole. If no matching
-     * is found or whenever a critical pair is trivial null is returned.
-     */
-    private List<Term> criticalPair(TRS trs, int r1index, int r2index) {
-        Substitution s = overlap(trs.queryRule(r1index).queryLeftSide(), trs.queryRule(r2index).queryLeftSide());
-        if (s != null) {
-            Substitution sigma = trs.queryRule(r1index).queryLeftSide().match(trs.queryRule(r1index).queryLeftSide());
-            Term left = trs.queryRule(r1index).queryRightSide().substitute(s).substitute(sigma);
-            List<Position> positions = trs.queryRule(r1index).queryLeftSide().queryAllPositions();
-            Term right = null;
-            for (Position pos : positions) {
-                if (trs.queryRule(r1index).queryLeftSide().substitute(s).querySubterm(pos).equals(trs.queryRule(r2index).queryLeftSide()))
-                    right = trs.queryRule(r1index).queryLeftSide().substitute(s).replaceSubterm(pos, trs.queryRule(r2index).queryRightSide());
-            }
-            if (left.equals(right)) return null;
-            List<Term> pair = new ArrayList<>();
-            pair.add(left);
-            pair.add(right);
-            return pair;
-        }
-        return null;
-    }
-
-    /**
-     * Finds a list of all the non-trivial critical pairs (t is not equivalent to s).
-     */
-    private List<List<Term>> criticalPairs(TRS trs) {
-        List<List<Term>> pairs = new ArrayList<>();
-        for (int i = 0; i < trs.queryRuleCount(); i++) {
-            for (int j = 0; j < trs.queryRuleCount(); j++) {
-                List<Term> pair = criticalPair(trs, i, j);
-                if (pair != null) pairs.add(pair);
-            }
-        }
-        return pairs;
-    }
-
     @Override
     public RESULT apply(TRS trs) {
         boolean left_linear = true;
@@ -104,7 +43,7 @@ public class Orthogonality extends StrategyInherit implements Strategy {
         for (int i = 0; i < trs.queryRuleCount(); i++)
             System.out.println(trs.queryRule(i));
 
-        System.out.println(criticalPairs(trs));
+        //System.out.println(criticalPairs(trs));
         if (criticalPairs(trs).isEmpty()) {
             System.out.println(RESULT.CONFLUENT);
             return RESULT.CONFLUENT;
