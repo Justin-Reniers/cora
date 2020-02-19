2b68cd24b137d48256d55db88fc5a56d59f5587b
Justin Reniers <justin-reniers@hotmail.com>
2020-02-19 22:44:39 +0100

    Added proper CriticalPair class
    Start to interchange `List<Term>` with `CriticalPair`

---
+++ src/main/java/cora/provingstrategies/CriticalPair.java
@@ -0,0 +1,32 @@
+package cora.provingstrategies;
+
+import cora.interfaces.terms.Term;
+
+public class CriticalPair {
+
+    private final Term left;
+    private final Term right;
+
+    /**
+     * Constructor for a CriticalPair. Creates a unmodifiable critical pair for the
+     * given left and right term.
+     */
+    public CriticalPair(Term left, Term right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    /**
+     * Returns the left term of the critical pair.
+     */
+    public Term getLeft() {
+        return left;
+    }
+
+    /**
+     * Returns the right term of the critical pair.
+     */
+    public Term getRight() {
+        return right;
+    }
+}
