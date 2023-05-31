78946d5c0bf65a27c14334d689538c33a0a1dde1
Justin-Reniers <justin-reniers@hotmail.com>
2023-05-31 13:15:19 +0200

    Undo command.

---
+++ src/main/java/cora/usercommands/UndoCommand.java
@@ -0,0 +1,56 @@
+package cora.usercommands;
+
+import cora.interfaces.smt.UserCommand;
+import cora.interfaces.terms.Position;
+import cora.interfaces.types.Type;
+import cora.smt.Equation;
+import cora.smt.EquivalenceProof;
+import cora.smt.ProofHistory;
+import cora.terms.Var;
+
+public class UndoCommand extends UserCommandInherit implements UserCommand {
+    private EquivalenceProof _proof;
+
+    public UndoCommand() {
+        super();
+    }
+
+    @Override
+    public Position queryPosition() {
+        return null;
+    }
+
+    @Override
+    public boolean applicable() {
+        return _proof.getLastCommand() != null;
+    }
+
+    @Override
+    public void apply() {
+        ProofHistory ph = _proof.getPreviousState();
+        _proof.clearEquations();
+        for (Equation eq : ph.getEquations()) _proof.addEquation(new Equation(eq));
+        _proof.setCurrentEquation();
+        _proof.emptyCompletenessEquationSet();
+        for (Equation ceq : ph.getCompletenessEquations()) _proof.addCompletenessEquation(new Equation(ceq));
+        _proof.setCompleteness(ph.getCompleteness());
+        _proof.setBottom(ph.getBottom());
+        _proof.setLcTrs(ph.getLcTrs());
+        _proof.deletePreviousState();
+    }
+
+    @Override
+    public void setProof(EquivalenceProof proof) {
+        _proof = proof;
+    }
+
+    @Override
+    public Var getFreshVar(Type expectedType) {
+        return null;
+    }
+
+    @Override
+    public EquivalenceProof getProof() {
+        return _proof;
+    }
+}
