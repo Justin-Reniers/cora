433d5899ea3dc4323ede1743f12b655deb620359
Justin-Reniers <justin-reniers@hotmail.com>
2023-08-10 10:47:37 +0200

    Worked out UnsatException

--- src/main/java/cora/exceptions/UnsatException.java
+++ src/main/java/cora/exceptions/UnsatException.java
@@ -1,2 +1,9 @@
-package cora.exceptions;public class UnsatException {
+package cora.exceptions;
+
+import cora.interfaces.terms.Term;
+
+public class UnsatException extends Error {
+    public UnsatException(String s) {
+        super("Constraint " + s + " is not satisfiable");
+    }
 }
